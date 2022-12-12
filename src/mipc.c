32e3ba6780eabec377e66696b692b9b9c24102c7
teddyxiong53 <1073167306@qq.com>
2022-12-12 19:24:20 +0800

    Add basic files.

---
+++ src/mipc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "mipc.h"
+#include "mylog.h"
+struct mipc_handle* mipc_create()
+{
+    return NULL;
+}
---
+++ src/mipc.h
@@ -0,0 +1,11 @@
+#ifndef __MIPC_H__
+#define __MIPC_H__
+
+
+struct mipc_handle {
+    int a;
+};
+
+struct mipc_handle * mipc_create();
+
+#endif // __MIPC_H__
